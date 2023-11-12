0c9991479286c57eb85b62681d23f9d11854b725
danu <danujagreru@gmail.com>
2023-11-12 09:55:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//import express from 'express';
+const express = require('express');
+const app = express();
+
+//declare a port number
+const port = 3000;
+
+//declare a route for GET /
+app.get('/', (req, res) => {
+    console.log('Hello Express!');
+    res.send("<h1>Hello Express!</h1>");
+})
+
+//open server on port 3000
+//callback function is executed when server is opened
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
