fb968b5b1842a6fe8a46e3bed23c8fa2196fb312
danu <danujagreru@gmail.com>
2023-11-12 14:08:34 +0530

    add try catch

--- controller/customerController.js
+++ controller/customerController.js
@@ -2,44 +2,64 @@ const Customer = require("../model/customerModel");
 
 module.exports = {
     getAll: async (req, res) => {
-        const customers = await Customer.find({});
+        try {
+            const customers = await Customer.find({});
 
-        res.status(200).json({
-            success: true,
-            data: customers
-        });
+            res.status(200).json({
+                success: true,
+                data: customers
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     },
     getById: async (req, res) => {
-        const customer = await Customer.findById(req.params.id);
+        try {
+            const customer = await Customer.findById(req.params.id);
 
-        res.status(200).json({
-            success: true,
-            data: customer
-        });
+            res.status(200).json({
+                success: true,
+                data: customer
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     },
     create: async (req, res) => {
-        const customer = await Customer.create(req.body);
+        try {
+            const customer = await Customer.create(req.body);
 
-        res.status(200).json({
-            success: true,
-            data: customer
-        });
+            res.status(200).json({
+                success: true,
+                data: customer
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     },
     update: async (req, res) => {
-        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body);
+        try {
+            const customer = await Customer.findByIdAndUpdate(req.params.id, req.body);
 
-        const updatedCustomer = await Customer.findById(req.params.id);
-        res.status(200).json({
-            success: true,
-            data: updatedCustomer
-        });
+            const updatedCustomer = await Customer.findById(req.params.id);
+            res.status(200).json({
+                success: true,
+                data: updatedCustomer
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     },
     delete: async (req, res) => {
-        const customer = await Customer.findByIdAndDelete(req.params.id);
+        try {
+            const customer = await Customer.findByIdAndDelete(req.params.id);
 
-        res.status(200).json({
-            success: true,
-            data: customer
-        });
+            res.status(200).json({
+                success: true,
+                data: customer
+            });
+        } catch (e) {
+            console.log(e.message);
+        }
     }
 }
