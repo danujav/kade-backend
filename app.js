16e60cc97ff8d11a3b67b86e51fc885d0cfdfd29
danu <danujagreru@gmail.com>
2023-11-12 10:44:39 +0530

    use middleware

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const app = express();
 //declare a port number
 const port = 3000;
 
+//middleware to parse json data
+app.use(express.json());
+
 //declare a route for GET /
 app.get('/', (req, res) => {
-    console.log('Hello Express!');
-    res.send("<h1>Lamai!</h1>");
-})
+    // console.log('Hello Express!');
+    res.send("Hello Express!");
+});
+
+app.post('/', (req, res) => {
+   /* console.log(req.body.id);
+    console.log(req.body.name);*/
+    console.log(req.body)
+   res.send(req.body);
+});
 
 //open server on port 3000
 //callback function is executed when server is opened
